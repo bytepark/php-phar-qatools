930f7debdce06b6aacf79bbf4dc6f05d590439ec
bytepark GmbH <code@bytepark.de>
2014-05-02 13:14:00 +0200

    add bash script to generate version information for README.md

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ php-phar-qatools
 ================
 
 Holds different php qa tools as phar file
+
+Included tools:
+
+* PHP_Depend 1.1.0
+* phpcb 1.1.0
+* phpcpd 2.0.1
+* phpDocumentor 2.4.0
+* phpDox 0.6.5
+* phploc 2.0.5
+* PHPMD 1.5.0
+* PHPUnit 4.0.19/
---
+++ versions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# collects all version information and puts them into the README
+
+return=""
+
+# hack for phpcb which does not have a working version string
+phpcbversion=`ls phpcb* |sed "s/phpcb-//" |sed "s/.phar//"`
+
+# use all tools to get their version string
+for tool in `ls *.phar`; do
+    output=`php $tool --version | sed "s/ version//" |cut -d " " -f 1-2 | sed "s/\r//"`
+    if [ "$output" == "Console Tool" ]; then
+	return="${return}\n* phpcb ${phpcbversion}"
+    else
+	return="${return}\n* ${output}"
+    fi
+done
+
+sed -i '/tools:/,$d' README.md
+
+echo -e  "Included tools:\n$return/" >> README.md
+

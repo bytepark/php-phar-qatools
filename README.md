343616ec130753415b8545e09727d92f3b4ec219
bytepark GmbH <code@bytepark.de>
2014-05-02 13:47:32 +0200

    add PHP CS Fixer by Fabien Potencier

--- README.md
+++ README.md
@@ -5,14 +5,15 @@ Holds different php qa tools as phar file
 
 Included tools:
 
-* PHP_Depend 1.1.0
-* phpcb 1.1.0
-* PHP Code Beautifier and Fixer 2.0.0a2
-* phpcpd 2.0.1
-* PHP_CodeSniffer 2.0.0a2
-* phpdcd 1.0.2
-* phpDocumentor 2.4.0
-* phpDox 0.6.5
-* phploc 2.0.5
-* PHPMD 1.5.0
-* PHPUnit 4.1.0
+* PHP_Depend 1.1.0 (pdepend.phar)
+* phpcb 1.1.0 (phpcb-1.1.0.phar)
+* PHP Code Beautifier and Fixer 2.0.0a2 (phpcbf.phar)
+* phpcpd 2.0.1 (phpcpd.phar)
+* PHP CS Fixer 0.4-DEV (php-cs-fixer.phar)
+* PHP_CodeSniffer 2.0.0a2 (phpcs.phar)
+* phpdcd 1.0.2 (phpdcd.phar)
+* phpDocumentor 2.4.0 (phpDocumentor.phar)
+* phpDox 0.6.5 (phpdox.phar)
+* phploc 2.0.5 (phploc.phar)
+* PHPMD 1.5.0 (phpmd.phar)
+* PHPUnit 4.1.0 (phpunit.phar)
--- versions.sh
+++ versions.sh
@@ -11,13 +11,15 @@ for tool in `ls *.phar`; do
     output=`php $tool --version | sed "s/ version//" |cut -d " " -f 1-2`
 
     if [ "$output" == "Console Tool" ]; then
-	return="${return}\n* phpcb ${phpcbversion}"
+	return="${return}\n* phpcb ${phpcbversion} (${tool})"
     elif [ "$tool" == "phpcbf.phar" ]; then
 	line=`echo "${output}" | sed "s/PHP_CodeSniffer/PHP Code Beautifier and Fixer/"`
-	return="${return}\n* ${line}"
-	x=1
+	return="${return}\n* ${line} (${tool})"
+    elif [ "$tool" == "php-cs-fixer.phar" ]; then
+	line=`php $tool --version | sed "s/ version//" | cut -d " " -f 1-4`
+	return="${return}\n* ${line} (${tool})"
     else
-	return="${return}\n* ${output}"
+	return="${return}\n* ${output} (${tool})"
     fi
 done
 

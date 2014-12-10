2ca7438d4420afb7be1abfbb144697d5fef2cd78
fbatschi <fbatschi@@users.noreply.github.com>
2014-12-10 23:22:50 +0100

    remove colors from --version output, which resulted in markdown problems

--- README.md
+++ README.md
@@ -9,13 +9,13 @@ It creates symlinks to all tools into /usr/local/bin.
 Included tools:
 
 * PDepend 2.0.4 (*pdepend.phar*)
-* PHP_CodeBrowser 1.1.1 (*phpcb-1.1.1.phar*)
 * PHP Code Beautifier and Fixer 2.0.0 (*phpcbf.phar*)
+* PHP_CodeBrowser 1.1.1 (*phpcb.phar*)
 * phpcpd 2.0.1 (*phpcpd.phar*)
-* [32mPHP CS Fixer[39m [33m1.2[39m (*php-cs-fixer.phar*)
+* PHP CS Fixer 1.2 (*php-cs-fixer.phar*)
 * PHP_CodeSniffer 2.0.0 (*phpcs.phar*)
 * phpdcd 1.0.2 (*phpdcd.phar*)
-* [32mphpDocumentor[39m [33mv2.8.1[39m (*phpDocumentor.phar*)
+* phpDocumentor v2.8.1 (*phpDocumentor.phar*)
 * phpDox 0.7.0 (*phpdox.phar*)
 * phploc 2.0.6 (*phploc.phar*)
 * PHPMD 2.1.3 (*phpmd.phar*)
--- tools/generate_versions.sh
+++ tools/generate_versions.sh
@@ -3,20 +3,15 @@
 
 return=""
 
-# hack for phpcb which does not have a working version string
-phpcbversion=`ls phpcb-* |sed "s/phpcb-//" |sed "s/.phar//"`
-
 # use all tools to get their version string
 for tool in `ls *.phar`; do
-    output=`php $tool --version | sed "s/ version//" |cut -d " " -f 1-2`
+    output=`php $tool --version | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|G|K]//g" | sed "s/ version//" |cut -d " " -f 1-2`
 
-    if [ "$output" == "Console Tool" ]; then
-	return="${return}\n* phpcb ${phpcbversion} (*${tool}*)"
-    elif [ "$tool" == "phpcbf.phar" ]; then
+    if [ "$tool" == "phpcbf.phar" ]; then
 	line=`echo "${output}" | sed "s/PHP_CodeSniffer/PHP Code Beautifier and Fixer/"`
 	return="${return}\n* ${line} (*${tool}*)"
     elif [ "$tool" == "php-cs-fixer.phar" ]; then
-	line=`php $tool --version | sed "s/ version//" | cut -d " " -f 1-4`
+	line=`php $tool --version --no-ansi | sed "s/ version//" | cut -d " " -f 1-4`
 	return="${return}\n* ${line} (*${tool}*)"
     else
 	return="${return}\n* ${output} (*${tool}*)"
